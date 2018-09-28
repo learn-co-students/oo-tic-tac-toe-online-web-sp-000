e67b84845469a740d14ba33fb6d49582db891d12
Douglas Uretsky <douglasuretsky@gmail.com>
2018-09-28 21:06:46 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,144 @@
+class TicTacToe
+  attr_accessor :board
+  
+  def initialize
+    @board = Array.new(9) {" "}
+  end
+  
+  def play
+    until over? || won?
+      turn
+    end
+    puts "Cat's Game!" if draw?
+    puts "Congratulations X!" if winner == "X"
+    puts "Congratulations O!" if winner == "O"
+  end
+  
+  def winner
+    hold_array = []
+    
+    WIN_COMBINATIONS.each do |combo|
+      combo.each do |index|
+        hold_array << board[index] if position_taken?(index)
+        end
+      if hold_array.length == 3 && hold_array.uniq.length == 1
+        winner = hold_array.uniq.first
+        return winner
+        else
+        hold_array.clear
+      end
+    end
+    nil
+  end
+  
+  def over?
+    return true if draw?
+    return true if won?
+  end
+  
+    def won?
+
+    hold_array = []
+  
+    WIN_COMBINATIONS.each do |combo|
+      combo.each do |index|
+        hold_array << board[index] if position_taken?(index)
+        end
+      if hold_array.length == 3 && hold_array.uniq.length == 1
+        return combo
+      else
+        hold_array.clear
+      end
+    end
+    false
+  end
+  
+   def full?
+    board.all? { |pos| pos == "X" || pos == "O" }
+  end
+  
+  def draw?
+    if full? && !won?
+      return true
+    else
+      return false
+    end
+  end
+  
+  def current_player(board)
+    count = turn_count(board)
+    return "X" if count.even?
+    return "O" if count.odd?
+  end
+
+  def player_move(index, token)
+    board[index] = token
+  end
+  
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    token = current_player
+    
+    unless valid_move?(index)
+      turn
+    else
+      move(index, token)
+      display_board
+    end
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2], # Top row
+    [3,4,5],  # Middle row
+    [6,7,8], # Bottom row
+    [0,4,8], # Diag. right to left
+    [6,4,2], # Diag. left to right
+    [0,3,6], # Left vertical
+    [7,4,1], # Mid vertical
+    [2,5,8] # Right vertical
+  ]
+  
+  def display_board
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  def input_to_index(input)
+    int = input.to_i
+    int - 1
+  end
+  
+  def move(index, char = "X")
+    board[index] = char
+  end
+  
+  def position_taken?(index)
+    !(board[index].nil? || board[index] == " ")
+  end
+  
+  def turn_count
+  count = 0
+  board.each do |pos|
+    count += 1 unless pos == " "
+  end
+  count
+end
+  
+  def current_player
+    count = turn_count
+    return "X" if count.even?
+    return "O" if count.odd?
+  end
+
+  def valid_move?(index)
+    if index.to_i.between?(0, 8)
+      return true if board[index] == " "
+    end
+      return false
+    end
+  end
