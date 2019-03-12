917f115fbd4e440f23825398780146205ebe064d
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-03-12 18:21:24 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,63 @@
+class TicTacToe
+
+  def initialize(board = nil)
+      @board = board || Array.new(9, " ")
+    end
+    WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def display_board
+   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+   puts "-----------"
+   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+   puts "-----------"
+   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+ end
+
+ def input_to_index(a_string)
+   a_string.to_i - 1
+ end
+
+ def move(index, player_token ="X")
+   @board[index]=player_token
+ end
+
+ def position_taken?(index)
+  if @board[index] == "X"|| @board[index] == "O"
+    return true
+  else false
+  end
+end
+
+  def  valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+end
+
+
+def turn_count(index)
+  counter = 0
+    if index == "X"|| index == "O"
+      counter += 1
+    end
+    return counter
+end
+
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(index)
+    move(index, player_token ="X")
+  else
+    turn(index)
+  end
+end
