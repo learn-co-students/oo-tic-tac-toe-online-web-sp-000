d03e02e1259f747f0991b1c078e4795346c32e51
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-03-13 01:08:25 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,10 +72,8 @@ end
 end
 
 def won?
-     WIN_COMBINATIONS.detect do|index| 
-     return index
-   end
-   return
-   false
- end
- end
+  WIN_COMBINATIONS.detect do|combo|
+    @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] 
+  end
+end
+end
