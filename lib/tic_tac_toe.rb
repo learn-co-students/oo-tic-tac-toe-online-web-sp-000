481d2b6801f318e7308c3987feb5d99dd3b59cb9
Connor Cowan <ccowan003@comcast.net>
2019-07-01 19:51:12 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,50 +11,51 @@ WIN_COMBINATIONS = [
   [1,4,7],
   [2,5,8],
 ]
-def initialize
-  @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def initialize(board = nil)     #default parameter set to nil if board is empty
+  @board = board || Array.new(9, " ")       #instance var equal to argument, init array to 9 empty spaces
+
 end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
 def input_to_index(user_input)
   index = user_input.to_i - 1
 end
 
-def move(board, index, value)
-  board[index] = value
+def move(index, value)
+  @board[index] = value
 end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+def position_taken?(index)
+  @board[index] != " " && @board[index] != ""
 end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
 end
 
-def turn(board)
+def turn
   puts "Please enter 1-9:"
-  input = gets.strip                #create local variable 'input' that gets input from player. 'strip' eliminates whitespace.
-  index = input_to_index(input)     #convert input to correct index with method call (#input_to_index(input))
-  if valid_move?(board, index)      #if move is valid (allowed)
-    move(board, index, current_player(board))              #call to #move making user move for index, and current_player for X or O!
-    display_board(board)
+  input = gets.strip
+  index = input_to_index(input)
+  char = current_player
+  if valid_move?(index)
+    move(index, char)
+    display_board
   else
-    turn(board)                     #reiterate turn if previous turn failed
+    turn
   end
-  #display_board(board)            #show the board
 end
 
-def turn_count(board)
+def turn_count
   turns = 0                             #initialize turns variable starting at 0
-  board.each do |token|                 #iterate each element on board. token is variable label for our array to be passed into the block of code
+  @board.each do |token|                 #iterate each element on board. token is variable label for our array to be passed into the block of code
     if token == "X" || token == "O"     #if tokens are either X or O add to turn count
       turns += 1
     end
@@ -62,48 +63,57 @@ def turn_count(board)
   return turns                           #returns turns. Listed in lab requirements. "Control return value of a method that uses iteraton in body"
 end                                      #This return could also be just: turns. However including 'returns' helps understanding
 
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"    #call #turn_count. Ternary operators ? : looks for true/false values then executes
+def current_player
+  turn_count % 2 == 0 ? "X" : "O"    #call #turn_count. Ternary operators ? : looks for true/false values then executes
 end                                         #using a modulo. if condition is true ? then "X" : otherwise value "O"
 
-def won?(board)     #check the board return true if win, false if not
+def won?     #check the board return true if win, false if not
 
-  WIN_COMBINATIONS.detect do |combo|          #iterating (detecting) CONSTANT for winning combos
-    board[combo[0]] == board[combo[1]] &&     #conditions for a WIN
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])           #takes board and index as arguments, returns true if position empty
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
+    end
+      false
   end
 end
 
-def full?(board)          #accepts board, returns true if every element in the board contains either an "X" or an "O"
-  board.all? do |token|   #iterator: all? block passed to it must return true for every iteration
-    token == "X" ||       #true for only X and O's
-    token == "O"
+def full?
+  @board.all? do |token| token != " "
   end
 end
 
-def draw?(board)
-  !won?(board) && full?(board)      #call to methods if board NOT won, but is FULL! Returns true only in those cases!
+def draw?
+  !(won?) && full?      #calls to methods
 end
 
-def over?(board)
-    won?(board) || draw?(board) || full?(board)
+def over?
+    won? || full? || draw?
 end
 
-def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
+def winner
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return "X"
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return "O"
+    else
+      nil
+    end
   end
+
 end
 
-def play(board)
-  while !over?(board)
-    turn(board)
+def play
+  while over? == false
+    turn
   end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
     puts "Cat's Game!"
   end
-#end
+end
+
 end
