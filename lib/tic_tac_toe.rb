eeabd73f90e4758bb1d9a4aea1b4a9e2eef48cd1
Jason J Cruz <jaycru06@yahoo.com>
2019-12-06 23:48:54 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,8 @@ class TicTacToe
   
   def won? 
     WIN_COMBINATIONS.detect do |win_combo| 
-      if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X") || (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
+      if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X") || 
+         (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
         win_combo
       else 
         false 
