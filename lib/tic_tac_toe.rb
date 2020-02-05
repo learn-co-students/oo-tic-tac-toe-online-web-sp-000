2ef1912c6ac1e63fbf82639b0ba9043ab0e9e39d
Aoune Syed <aoune007@gmail.com>
2020-02-05 06:01:32 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,5 @@
 class TicTacToe
 
-attr_accessor :board
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
@@ -17,14 +16,16 @@ attr_accessor :board
   ]
 
   def display_board(board)
-    separator = "|"
-    lines = "-----------"
 
-    puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
-    puts "#{lines}"
-    puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
-    puts "#{lines}"
-    puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+    @board
+    #separator = "|"
+    #lines = "-----------"
+
+    #puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+    #puts "#{lines}"
+    #puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+    #puts "#{lines}"
+    #puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
   end
 
 end
