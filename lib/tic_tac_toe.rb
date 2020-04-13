84192337d30d92ee3e186c429f52567f1bc54a08
C. Leow <chienleow1@gmail.com>
2020-04-13 19:24:35 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,9 +4,43 @@ class TicTacToe
     
   
   def initialize(board = nil)
-    @board = board || Array.new(9, " ")
-  
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    # @board = board || Array.new(9, " ")
   end
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(user_input)
+    user_input = user_input.to_i - 1
+      return user_input
+  end
+  
+  def move(index, character)
+    @board[index] = character
+  end
+  
+  def position_taken?(index)
+    if @board[index] == " "
+      return false
+    else
+      return true
+    end
+  end
+  
+  def valid_move?(index)
+    if index.between?(0,8) && !position_taken?(index)
+      return true
+    else
+      return false
+    end
+  end
+  
 end
 
 
