0f696fa2b30620179d0cc75f9bb7717068ea5984
C. Leow <chienleow1@gmail.com>
2020-04-13 20:09:35 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,6 +70,22 @@ class TicTacToe
       end
   end
   
+  def won?
+    WIN_COMBINATIONS.each do |win_combination| 
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+   
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+   
+      if position_1 == position_2 && position_2 == position_3 && position_1 != " "
+        return win_combination
+      end
+    end
+    false
+  end
   
 end
 
