ae9bc070daa8c77087483f2e59d40942a82641f5
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-06-05 01:38:50 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -93,4 +93,15 @@ class TicTacToe
     end
   end
 
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
 end
