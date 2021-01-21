9d2de64bc284b03a4db3bb1ea824641f7d60196d
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-21 18:53:31 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,6 @@
 class TicTacToe
-attr_accessor :board,:TicTacToe 
-
-
+  attr_accessor :board,:TicTacToe 
+  
 WIN_COMBINATIONS = [ 
 [0, 1, 2],
 [3, 4, 5],
@@ -12,14 +11,29 @@ WIN_COMBINATIONS = [
 [0, 4, 8],
 [6, 4, 2]
 ] 
+  
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+ 
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+ 
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+ 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
- 
-  def initialize
-    @board = [" "," "," "," "," "," "," "," "," "] 
-  end 
- 
- 
-def play(board)
+
+def play
   puts "Please select a number" 
   user_input = gets.chomp
   game = TicTacToe.new 
@@ -31,13 +45,6 @@ def over?
 end 
 
 
-def display_board
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "------------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "------------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end 
 
 def input_to_index(user_input)
   index = user_input.to_i 
@@ -47,13 +54,13 @@ end
 
 
 def move(board, array_index) 
-  puts board [0, "X"]
+  puts board[0, "X"]
   puts board[4, "O"]
 end 
-
+ 
 
  
-def position_taken?(board)
+def position_taken?
     if board[index] == nil || board[index] == ""
       false 
       true 
@@ -66,16 +73,6 @@ def valid_move?
 end 
  
  
- 
- def turn_count
-end 
-
-
-def current_player
-  
-end 
-
-
 def turn 
   input_to_index
   valid_move?
@@ -106,8 +103,5 @@ def over?
     puts "You won"
   end
 end 
-
-
-
-
 end 
+ 
