942ff98e7d03a5b420f469f41db2cee1bfb8f1fd
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-21 02:13:43 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,9 +33,9 @@ end
 
 def display_board
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-
+  puts "------------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
- 
+  puts "------------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end 
 
@@ -46,7 +46,68 @@ end
 
 
 
-def move(board, index) 
+def move(board, array_index) 
+  puts board [0, "X"]
+  puts board[4, "O"]
 end 
+
+
+ 
+def position_taken?(board)
+    if board[index] == nil || board[index] == ""
+      false 
+      true 
+    end 
+end 
+
+
+def valid_move?
+  index.between?(0,8) && !position_taken(board, index)
 end 
  
+ 
+ 
+ def turn_count
+end 
+
+
+def current_player
+  
+end 
+
+
+def turn 
+  input_to_index
+  valid_move?
+  current_player
+end 
+
+
+def won?
+  
+end 
+
+
+def full?
+  
+end 
+
+
+def draw?
+end 
+
+
+def over?
+  if game.draw?
+    puts "Game is over"
+  end 
+  
+  if game.won?
+    puts "You won"
+  end
+end 
+
+
+
+
+end 
